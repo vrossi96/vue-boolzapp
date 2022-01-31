dd2ec5dc583a6bac287fabbf2ca13729a1678ec0
vrossi96 <vrossi396@yahoo.it>
2022-01-31 10:41:14 +0100

    added files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const app = new Vue({
+   el: "#root",
+   data: {
+      user: {
+         name: "Nome Utente",
+         avatar: "_io",
+      },
+      contacts: [
+         {
+            name: "Michele",
+            avatar: "_1",
+         },
+         {
+            name: "Fabio",
+            avatar: "_2",
+         },
+         {
+            name: "Samuele",
+            avatar: "_3",
+         },
+         {
+            name: "Luisa",
+            avatar: "_4",
+         },
+      ],
+   },
+});
