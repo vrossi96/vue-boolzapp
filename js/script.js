85adc42b7002dfc9407960bdff4a36e3a4423046
vrossi96 <vrossi396@yahoo.it>
2022-02-01 14:01:05 +0100

    min fix

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ const app = new Vue({
                {
                   date: "20/03/2020 16:35:00",
                   text: "Mi piacerebbe ma devo andare a fare la spesa.",
-                  status: "received",
+                  status: "sent",
                },
             ],
          },
