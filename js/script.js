c8ed9394a1420cff3e4486737e4f42221ba00d70
vrossi96 <vrossi396@yahoo.it>
2022-02-01 13:12:57 +0100

    linked visible contact in header

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ const app = new Vue({
          {
             name: "Fabio",
             avatar: "_2",
-            visible: true,
+            visible: false,
             messages: [
                {
                   date: "20/03/2020 16:30:00",
@@ -53,7 +53,7 @@ const app = new Vue({
          {
             name: "Samuele",
             avatar: "_3",
-            visible: true,
+            visible: false,
             messages: [
                {
                   date: "28/03/2020 10:10:40",
@@ -75,7 +75,7 @@ const app = new Vue({
          {
             name: "Marco",
             avatar: "_4",
-            visible: true,
+            visible: false,
             messages: [
                {
                   date: "10/01/2020 15:30:55",
@@ -90,5 +90,10 @@ const app = new Vue({
             ],
          },
       ],
+   },
+   methods: {
+      selectContact(index) {
+         this.contacts[index].visible = !this.contacts[index].visible;
+      },
    },
 });
